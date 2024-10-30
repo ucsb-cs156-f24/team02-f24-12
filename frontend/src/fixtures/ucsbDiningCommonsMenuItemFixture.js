9f4d781fc6f624c33bc080c628d54b227b35e707
Yixuan Wong <yixuanwong@ucsb.edu>
2024-10-29 20:27:16 -0700

    yx - fixed formatting for ucsbDiningCommonsMenuItemFixture.js

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixture.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixture.js
@@ -1,31 +1,31 @@
 const ucsbDiningCommonsMenuItemFixtures = {
-    oneMenuItem: {
+  oneMenuItem: {
+    id: 1,
+    diningCommonsCode: "DLG",
+    name: "DeLaGuerra",
+    station: "Grill",
+  },
+  threeMenuItems: [
+    {
       id: 1,
       diningCommonsCode: "DLG",
       name: "DeLaGuerra",
       station: "Grill",
     },
-    threeMenuItems: [
-      {
-        id: 1,
-        diningCommonsCode: "DLG",
-        name: "DeLaGuerra",
-        station: "Grill",
-      },
-      {
-        id: 2,
-        diningCommonsCode: "CRL",
-        name: "Carillo",
-        station: "Bakery",
-      },
-      {
-        id: 3,
-        diningCommonsCode: "PRT",
-        name: "Portola",
-        station: "Dessert",
-      },
-    ],
-  };
+    {
+      id: 2,
+      diningCommonsCode: "CRL",
+      name: "Carillo",
+      station: "Bakery",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "PRT",
+      name: "Portola",
+      station: "Dessert",
+    },
+  ],
+};
    
-  export { ucsbDiningCommonsMenuItemFixtures };
+export { ucsbDiningCommonsMenuItemFixtures };
   
