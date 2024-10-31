dc4f9082f793891825eadf9aba3432c42aa58576
yungong <yungong@ucsb.edu>
2024-11-01 07:24:23 +0800

    yungong - finished creating fixtures for Articles

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,5 +1,14 @@
 const articlesFixtures = {
-    oneArticle: {
+  oneArticle: {
+    id: 1,
+    title: "FirstArticle",
+    url: "https://www.google.com/",
+    explanation: "This is my first article",
+    email: "first@ucsb.edu",
+    dateAdded: "2022-01-02T12:00:00",
+  },
+  threeArticles: [
+    {
       id: 1,
       title: "FirstArticle",
       url: "https://www.google.com/",
@@ -7,33 +16,24 @@ const articlesFixtures = {
       email: "first@ucsb.edu",
       dateAdded: "2022-01-02T12:00:00",
     },
-    threeArticles: [
-      {
-        id: 1,
-        title: "FirstArticle",
-        url: "https://www.google.com/",
-        explanation: "This is my first article",
-        email: "first@ucsb.edu",
-        dateAdded: "2022-01-02T12:00:00",
-      },
-      {
-        id: 2,
-        title: "SecondArticle",
-        url: "https://www.baidu.com/",
-        explanation: "This is my second article",
-        email: "second@ucsb.edu",
-        dateAdded: "2022-04-03T12:00:00",
-      },
-      {
-        id: 3,
-        title: "ThirdArticle",
-        url: "https://www.souhu.com/",
-        explanation: "This is my third article",
-        email: "third@ucsb.edu",
-        dateAdded: "2022-07-04T12:00:00",
-      },
-    ],
-  };
+    {
+      id: 2,
+      title: "SecondArticle",
+      url: "https://www.baidu.com/",
+      explanation: "This is my second article",
+      email: "second@ucsb.edu",
+      dateAdded: "2022-04-03T12:00:00",
+    },
+    {
+      id: 3,
+      title: "ThirdArticle",
+      url: "https://www.souhu.com/",
+      explanation: "This is my third article",
+      email: "third@ucsb.edu",
+      dateAdded: "2022-07-04T12:00:00",
+    },
+  ],
+};
   
-  export { articlesFixtures };
+export { articlesFixtures };
   
