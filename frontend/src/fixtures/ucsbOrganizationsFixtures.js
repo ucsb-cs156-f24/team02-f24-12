541fd04f5f904494081c644616da1fc0d2ce477d
Abby-Fan04 <yfan187@ucsb.edu>
2024-10-31 18:08:57 -0700

    add fixture

---
+++ frontend/src/fixtures/ucsbOrganizationsFixtures.js
@@ -0,0 +1,36 @@
+const ucsbOrganizationsFixtures = {
+    oneOrganization: [
+      {
+        orgcode: "ZPR",
+        orgTranslationShort: "ZETA PHI RHO",
+        orgTranslation: "ZETA PHI RHO",
+        inactive: false,
+      },
+    ],
+  
+    threeOrganizations: [
+      {
+        orgcode: "SKY",
+        orgTranslationShort: "SKYDIVING CLUB",
+        orgTranslation: "SKYDIVING CLUB AT UCSB",
+        inactive: false,
+      },
+  
+      {
+        orgcode: "OSLI",
+        orgTranslationShort: "STUDENT LIFE",
+        orgTranslation: "OFFICE OF STUDENT LIFE	",
+        inactive: false,
+      },
+  
+      {
+        orgcode: "KRC",
+        orgTranslationShort: "KOREAN RADIO CL",
+        orgTranslation: "KOREAN RADIO CLUB",
+        inactive: false,
+      },
+    ],
+  };
+  
+  export { ucsbOrganizationsFixtures };
+  
