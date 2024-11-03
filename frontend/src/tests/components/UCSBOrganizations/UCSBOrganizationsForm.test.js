3489c6b7b461b6f11cc3a0343a42807a78cb1999
Abby-Fan04 <yfan187@ucsb.edu>
2024-11-03 00:24:08 -0700

    add three files

---
+++ frontend/src/tests/components/UCSBOrganizations/UCSBOrganizationsForm.test.js
@@ -0,0 +1,99 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import UCSBOrganizationsForm from "main/components/UCSBOrganizations/UCSBOrganizationsForm";
+import { restaurantFixtures } from "fixtures/ucsbOrganizationsFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedNavigate,
+}));
+
+describe("UCSBOrganizationsForm tests", () => {
+  const queryClient = new QueryClient();
+
+  const expectedHeaders = ["orgcode", "orgTranslationShort", "orgTranslation", "inactive?"];
+  const testId = "UCSBOrganizationsForm";
+
+  test("renders correctly with no initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationsForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+  });
+
+  test("renders correctly when passing in initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationsForm initialContents={ucsbOrganizationsFixtures.oneOrganization} />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expect(await screen.findByTestId(`${testId}-orgcode`)).toBeInTheDocument();
+    expect(screen.getByText(`orgcode`)).toBeInTheDocument();
+  });
+
+  test("that navigate(-1) is called when Cancel is clicked", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationsForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+    expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+    const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+  });
+
+  test("that the correct validations are performed", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationsForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+    const submitButton = screen.getByText(/Create/);
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/orgTranslationShort is required/);
+    expect(screen.getByText(/orgTranslation is required/)).toBeInTheDocument();
+
+    // const orgTranslationShortInput = screen.getByTestId(`${testId}-orgTranslationShort`);
+    // fireEvent.change(orgTranslationShortInput, { target: { value: "a".repeat(31) } });
+    // fireEvent.click(submitButton);
+
+    // await waitFor(() => {
+    //   expect(screen.getByText(/Max length 30 characters/)).toBeInTheDocument();
+    // });
+  });
+});
